d8dd03ecb2d1019f2da88f4777ee3329cfa0b4f5
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-04-27 03:40:00 -0300

    feat: add component link

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Banner from "./components/Banner/Index"
+import Link from "./components/Link"
 
 function App() {
 
   return (
-    <Banner src="../public/banner-desktop.png" alt="Banner" />
+    <>
+      <Banner src="../public/banner-desktop.png" alt="Banner" />
+      <Link href="/" target="_blank">Link</Link>
+    </>
   )
 }
 
