44a8c3ce2b202a09969df6dfdf4f7405678fe990
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-04-30 20:39:28 -0300

    feat: add component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner/Index"
+import Button from "./components/Button"
 import Link from "./components/Link"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Banner src="../public/banner-desktop.png" alt="Banner" />
       <Link href="/" target="_blank">Link</Link>
+      <Button variant="default">Botão</Button>
     </>
   )
 }
