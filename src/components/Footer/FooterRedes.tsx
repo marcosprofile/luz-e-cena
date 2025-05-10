1dcc794b510a561ca04af1d4feecc8e06462a5f6
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-05-10 01:37:59 -0300

    feat: update footer (redes icons)

--- src/components/Footer/FooterRedes.tsx
+++ src/components/Footer/FooterRedes.tsx
@@ -1,10 +1,22 @@
+import { FaInstagram, FaTiktok, FaWhatsapp } from 'react-icons/fa6'
 import styles from './Footer.module.css'
+import Link from '../Link'
 
 const FooterRedes = () => {
   return (
     <div>
       <h4 className={styles.titulo}>Siga nossas redes:</h4>
-      
+      <div className={styles.redes}>
+        <Link variant="branco">
+          <FaWhatsapp />
+        </Link>
+        <Link variant="branco">
+          <FaInstagram />
+        </Link>
+        <Link variant="branco">
+          <FaTiktok />
+        </Link>
+      </div>
     </div>
   )
 }
